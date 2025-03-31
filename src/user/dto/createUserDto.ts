dccbcf4a35bd51a01290d1b3bffc3753ebdedf9a
Amit @Hostologies <d3v@hostologies.com>
2025-03-31 23:34:49 +0600

    update

---
+++ src/user/dto/createUserDto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, IsNumberString, IsString } from "class-validator";
+
+export class createUserDto{
+    @IsString()
+    name: string;
+    @IsEmail()
+    email: string;
+    @IsNumberString()
+    password: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { createUserDto } from './dto/createUserDto';
+import { UserService } from './user.service';
+import { CommentService } from 'src/comment/comment.service';
 
 @Controller('user')
 export class UserController {
+
     @Get('all')
     findAll(){
         return 'This action returns a list of all users';
+    }
+
+    constructor(
+        private readonly userService:UserService,
+        private readonly commentServices: CommentService
+    ){}
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.userService.findOne(id)
+    }
+
+    @Post("/add-user")
+    create(@Body() createUserDto:createUserDto) {
+        return this.userService.create(createUserDto);
+    }
+
+    @Post(":id/comments")
+    getUserComments(@Param(`id`) id:string){
+        return this.commentServices.findUserComments(id)
     }
 }
